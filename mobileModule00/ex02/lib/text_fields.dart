62fd10b1e9f977d02d124bfdbcc7ebeeae284960
Claken <sachouam@student.42.fr>
2024-03-04 21:46:39 +0100

    textfields in progress

---
+++ mobileModule00/ex02/lib/text_fields.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextFields extends StatelessWidget {
+
+	final String userInput;
+	final String result;
+
+	const TextFields({
+		super.key,
+		required this.userInput,
+		required this.result,
+	});
+
+	@override
+	Widget build(BuildContext context) {
+		return 	SizedBox()
+	}
+
+}
