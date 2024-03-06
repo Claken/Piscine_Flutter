e9de0303e7c2b2a8fc1268a22801c79aae990492
Claken <sachouam@student.42.fr>
2024-03-07 00:05:27 +0100

    module01 in progress

---
+++ mobileModule01/weatherApp_proj/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              debugPrint("button pressed");
+            },
+            icon: const Icon(Icons.search),
+            color: Colors.white,
+          ),
+          toolbarHeight: 80.2,
+          backgroundColor: const Color.fromARGB(255, 78, 68, 107),
+          title: Text(
+            'Search location...',
+            style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Colors.white.withOpacity(0.1)),
+          ),
+          actions: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const VerticalDivider(color: Colors.white),
+                IconButton(
+                  onPressed: () {
+                    debugPrint("button pressed");
+                  },
+                  icon: const Icon( Icons.location_on, ),
+                  color: Colors.white,
+                ),
+              ],
+            )
+          ],
+        ),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
