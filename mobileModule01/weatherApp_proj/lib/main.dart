aa16c1ca1b6f94a51de8bbf483efa31d3f2cb067
sachouam <sachouam@student.42.fr>
2024-03-07 11:00:41 +0100

    module01 in progress : tabbar

--- mobileModule01/weatherApp_proj/lib/main.dart
+++ mobileModule01/weatherApp_proj/lib/main.dart
@@ -36,7 +36,9 @@ class MainApp extends StatelessWidget {
                   onPressed: () {
                     debugPrint("button pressed");
                   },
-                  icon: const Icon( Icons.location_on, ),
+                  icon: const Icon(
+                    Icons.location_on,
+                  ),
                   color: Colors.white,
                 ),
               ],
@@ -45,6 +47,28 @@ class MainApp extends StatelessWidget {
         ),
         body: const Center(
           child: Text('Hello World!'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: const Color.fromARGB(255, 78, 68, 107),
+          child: DefaultTabController(
+              length: 3,
+              child: Scaffold(
+                  appBar: AppBar(
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.rocket),),
+                    Tab(icon: Icon(Icons.rocket),),
+                    Tab(icon: Icon(Icons.rocket),)
+                  ]
+                ),
+              ),
+                body: TabBarView(children: [
+                  Container(color: Colors.white, child: const Icon(Icons.rocket)),
+                  Container(color: Colors.white, child: const Icon(Icons.rocket)),
+                  Container(color: Colors.white, child: const Icon(Icons.rocket)),
+                ],) 
+              )
+              ),
         ),
       ),
     );
