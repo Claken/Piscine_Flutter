9f57224a9035ad0308def6502fba5de2d8f7655e
sachouam <sachouam@student.42.fr>
2024-03-16 21:23:41 +0100

    ex01 normally done

--- mobileModule02/Modulel02/lib/searcher.dart
+++ mobileModule02/Modulel02/lib/searcher.dart
@@ -10,22 +10,19 @@ class CityInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        padding: const EdgeInsets.all(10),
-        itemCount: listOfCities?.length ?? 10,
-        itemBuilder: (BuildContext context, int index) {
-          return SizedBox(
-              height: 20,
-              child: Container(
-                child: listOfCities != null
-                    ? Text(
-                        '${listOfCities?[index]['name']}, ${listOfCities?[index]['admin1']}, ${listOfCities?[index]['country']} '
-                        
-                        )
-                    : const Text('wait for list'),
-              ));
-        },
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
-        );
+    return listOfCities != null
+        ? ListView.separated(
+            padding: const EdgeInsets.all(10),
+            itemCount: listOfCities?.length ?? 10,
+            itemBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                  height: 20,
+                  child: Text(
+                      '${listOfCities?[index]['name']}, ${listOfCities?[index]['admin1']}, ${listOfCities?[index]['country']} '));
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          )
+        : const Center(child: Text('no city found'));
   }
 }
