fc6c539cae291151b0f7d2413e17bd67c1bed677
Claken <sachouam@student.42.fr>
2024-03-23 21:52:34 +0100

    module03 in progress : 5 suggestions instead of 10

---
+++ mobileModule03/Module03/lib/page_currently.dart
@@ -0,0 +1,45 @@
+import 'package:ex00/error_message.dart';
+import 'package:flutter/material.dart';
+
+class CurrentlyPage extends StatelessWidget {
+  const CurrentlyPage({
+    super.key,
+    required this.coord,
+    required this.current,
+    required this.errorText,
+  });
+
+  final Map<String, String> coord;
+  final Map<String, String> current;
+  final String errorText;
+
+  @override
+  Widget build(BuildContext context) {
+    return errorText.isEmpty ? Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "${coord['cityName']}",
+          ),
+          Text(
+            "${coord['region']}",
+          ),
+          Text(
+            "${coord['country']}",
+          ),
+          Text(
+            "${current['temp']}",
+          ),
+          Text(
+            "${current['weather']}",
+          ),
+          Text(
+            "${current['wind']}",
+          ),
+        ]
+        )
+        : ErrorMessage(errorMessage: errorText,);
+        
+  }
+}
---
+++ mobileModule03/Module03/lib/page_weekly.dart
@@ -0,0 +1,41 @@
+import 'package:ex00/error_message.dart';
+import 'package:flutter/material.dart';
+
+class WeeklyPage extends StatelessWidget {
+  const WeeklyPage({
+    super.key,
+    required this.coord,
+    required this.weekly,
+    required this.errorText,
+
+  });
+
+  final Map<String, String> coord;
+  final Map<String, Map<String, String>> weekly;
+  final String errorText;
+
+  List<Widget> weeklyList() {
+    List<Widget> list = [];
+
+    list.add(Center(child: Text("${coord['cityName']}")));
+    list.add(Center(child: Text("${coord['region']}")));
+    list.add(Center(child: Text("${coord['country']}")));
+    list.addAll(weekly.entries.map(
+      (entry) => Text(
+        '${entry.value['date']}   ${entry.value['min']}    ${entry.value['max']}    ${entry.value['weather']}',
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+      ),
+    ));
+    return list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return errorText.isEmpty
+        ? ListView(children: weeklyList())
+        : ErrorMessage(
+            errorMessage: errorText,
+          );
+  }
+}
