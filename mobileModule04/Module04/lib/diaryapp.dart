054d6d8e279c1c47124b55d73945d63d4de809b4
Claken <sachouam@student.42.fr>
2024-04-07 01:37:45 +0200

    Auth0 in progress

--- mobileModule04/Module04/lib/diaryapp.dart
+++ mobileModule04/Module04/lib/diaryapp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auth0_flutter/auth0_flutter.dart';
 
 class Diary extends StatefulWidget {
   const Diary({super.key});
@@ -9,6 +10,8 @@ class Diary extends StatefulWidget {
 
 class _DiaryState extends State<Diary> {
   bool _isToggle = false;
+  bool _isBusy = false;
+  late String errorMessage;
 
   void _toggleButton() {
     setState(() {
@@ -18,32 +21,36 @@ class _DiaryState extends State<Diary> {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/book-with-yellow-cover.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-  child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const SizedBox(
-          child: Center(
-              child: Text('WELCOME TO YOUR DIARY',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w100,
-                  )))),
-      ElevatedButton(
-        style: TextButton.styleFrom(
-          backgroundColor: Colors.green,
-          side: const BorderSide(width: 1, color: Colors.green),
-          padding: const EdgeInsets.all(20),
-        ),
-        onPressed: () {
-          _toggleButton();
-        },
-        child: const Text('Login', style: TextStyle(color: Colors.white)),
-      )
-    ]));
+    return Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/book-with-yellow-cover.jpg"),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Center(
+                  child: Text('WELCOME TO YOUR\nDIARY',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        backgroundColor: Colors.brown
+                      ))),
+              ElevatedButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  side: const BorderSide(width: 1, color: Colors.green),
+                  padding: const EdgeInsets.all(20),
+                ),
+                onPressed: () {
+                  _toggleButton();
+                },
+                child:
+                    const Text('Login', style: TextStyle(color: Colors.white)),
+              )
+            ])));
   }
 }
