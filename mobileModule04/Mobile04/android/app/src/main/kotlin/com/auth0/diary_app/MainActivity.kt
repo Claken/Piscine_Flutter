6be42d86850996630a9c59f66a1c987cef64fcf6
sachouam <sachouam@student.42.fr>
2024-04-11 18:57:53 +0200

    login athentification works

--- mobileModule04/Mobile04/android/app/src/main/kotlin/com/auth0/diary_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.auth0.diary_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ mobileModule04/Module04/android/app/src/main/kotlin/com/auth0/diaryapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.auth0.diaryapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
